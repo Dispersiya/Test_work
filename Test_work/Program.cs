b22624e99d519648e786f25013f1674ad389d815
Vika <nika-maks@inbox.ru>
2022-05-09 19:26:29 +0300

    Выбор из массива строк меньше или равных 3 символом и заполнение ими другого массива

--- Test_work/Program.cs
+++ Test_work/Program.cs
@@ -4,3 +4,16 @@
 
 string[] array1 = new string[11] {"hello", "2", "world", ":-)", "1234", "15678","-2","computer science","Russia","Denmark","Kazan"};
 string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
